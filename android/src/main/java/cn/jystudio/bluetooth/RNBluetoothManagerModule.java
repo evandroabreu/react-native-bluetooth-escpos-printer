b2af79d38da30048fa77d40d838d7a3bada3b440
Evandro Abreu <evandro.abreu@gmail.com>
2022-01-18 20:17:01 -0300

    Update RNBluetoothManagerModule.java

--- android/src/main/java/cn/jystudio/bluetooth/RNBluetoothManagerModule.java
+++ android/src/main/java/cn/jystudio/bluetooth/RNBluetoothManagerModule.java
@@ -10,8 +10,9 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import android.util.Log;
 import android.widget.Toast;
 
